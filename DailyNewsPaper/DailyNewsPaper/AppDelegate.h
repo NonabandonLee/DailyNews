5ea92ec3b1db0ab907a400dbc3d996e8bf3b64f5
NonabandonLee <99685911@qq.com>
2015-11-12 01:34:50 +0800

    初始化代码仓库

---
+++ DailyNewsPaper/DailyNewsPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DailyNewsPaper
+//
+//  Created by ken Zhao on 15/11/12.
+//  Copyright © 2015年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
