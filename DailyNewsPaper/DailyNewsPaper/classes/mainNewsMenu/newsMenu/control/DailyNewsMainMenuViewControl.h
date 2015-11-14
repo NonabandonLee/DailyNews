4e06e8371dc60918a0b0551ebf6ba717044b7fd9
NonabandonLee <99685911@qq.com>
2015-11-14 14:06:03 +0800

    启动页面

---
+++ DailyNewsPaper/DailyNewsPaper/classes/mainNewsMenu/newsMenu/control/DailyNewsMainMenuViewControl.h
@@ -0,0 +1,13 @@
+//
+//  DailyNewsMainMenuViewControl.h
+//  DailyNewsPaper
+//
+//  Created by ken Zhao on 15/11/14.
+//  Copyright © 2015年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DailyNewsMainMenuViewControl : UIViewController
+
+@end
---
+++ DailyNewsPaper/DailyNewsPaper/classes/mainNewsMenu/newsMenu/control/DailyNewsMainMenuViewControl.m
@@ -0,0 +1,37 @@
+//
+//  DailyNewsMainMenuViewControl.m
+//  DailyNewsPaper
+//
+//  Created by ken Zhao on 15/11/14.
+//  Copyright © 2015年 ios. All rights reserved.
+//
+
+#import "DailyNewsMainMenuViewControl.h"
+
+@interface DailyNewsMainMenuViewControl ()
+
+@end
+
+@implementation DailyNewsMainMenuViewControl
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
